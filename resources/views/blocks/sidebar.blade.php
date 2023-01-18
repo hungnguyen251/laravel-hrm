cd3efeaf7c1fd25e0d330a7cb5575fc10d374a5b
Hung NGUYEN <102758735+hungnguyen251@users.noreply.github.com>
2023-01-18 17:17:02 +0700

    Fix UI navbar

--- resources/views/blocks/sidebar.blade.php
+++ resources/views/blocks/sidebar.blade.php
@@ -28,144 +28,151 @@
             </div>
         </div>
       <!-- Sidebar Menu -->
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
                 with font-awesome or any other icon font library -->
-                <li class="nav-item menu-open">
+                <li class="nav-item">
                     <a href="{{ route('dashboard') }}" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Trang thống kê</p>
                     </a>
                 </li>
 
-                <li class="nav-item menu-open">
+                <li class="nav-item">
                     <a href="{{ route('companies.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-building"></i>
                         <p>Thông tin công ty</p>
                     </a>
                 </li>
 
-                <li class="nav-item menu-open">
+                <li class="nav-item">
                     <a href="{{ route('staffs.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-user-friends"></i>
-                        <p>Nhân viên<i class="fas fa-angle-left right"></i></p>
+                        <p>Nhân viên</p>
                     </a>
-                    {{-- <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{ route('staffs.index') }}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Toàn thời gian</p>
-                            </a>
-                        </li>
-                    </ul>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Bán thời gian</p>
-                            </a>
-                        </li>
-                    </ul>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Thực tập sinh</p>
-                            </a>
-                        </li>
-                    </ul> --}}
                 </li>
 
-                <li class="nav-item menu-open">
+                <li class="nav-item">
+                    <a href="{{ route('staffs.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-user-friends"></i>
+                        <p>Danh sách<i class="fas fa-angle-left right"></i></p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('positions.index') }}" class="nav-link">
+                                <i class="nav-icon fas fa-house-user"></i>
+                                <p>Chức danh</p>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('diplomas.index') }}" class="nav-link">
+                                <i class="nav-icon fas fa-house-user"></i>
+                                <p>Bằng cấp</p>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('rewards.index') }}" class="nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>Giải thưởng</p>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('departments.index') }}" class="nav-link">
+                                <i class="nav-icon fas fa-house-user"></i>
+                                <p>Phòng ban</p>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('salaries.index') }}" class="nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>Tiền lương</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
                     <a href="{{ route('notifications.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-bell"></i>
                         <p>Thông báo</p>
                     </a>
                 </li>
 
-                <li class="nav-item menu-open">
+                <li class="nav-item">
                     <a href="{{ route('staffs.show', ['id' => Auth::user()->staff->id]) }}" class="nav-link">
                         <i class="nav-icon fas fa-user"></i>
                         <p>Thông tin cá nhân</p>
                     </a>
                 </li>
 
-                <li class="nav-item menu-open">
-                    <a href="{{ route('departments.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-house-user"></i>
-                        <p>Danh sách phòng ban</p>
+                <li class="nav-item">
+                    <a href="{{ route('staffs.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-user-friends"></i>
+                        <p>Kế toán <i class="fas fa-angle-left right"></i></p>
                     </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('timesheets.monthSelection') }}" class="nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>Bảng tính lương</p>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
 
-                <li class="nav-item menu-open">
-                    <a href="{{ route('positions.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-house-user"></i>
-                        <p>Danh sách chức danh</p>
-                    </a>
-                </li>
-
-                <li class="nav-item menu-open">
-                    <a href="{{ route('diplomas.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-house-user"></i>
-                        <p>Danh sách bằng cấp</p>
-                    </a>
-                </li>
-
-                <li class="nav-item menu-open">
-                    <a href="{{ route('rewards.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>Danh sách giải thưởng</p>
-                    </a>
-                </li>
-
-                <li class="nav-item menu-open">
+                <li class="nav-item">
                     <a href="{{ route('users.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-user-shield"></i>
                         <p>Danh sách tài khoản</p>
                     </a>
                 </li>
 
-                <li class="nav-item menu-open">
-                    <a href="{{ route('salaries.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>Danh sách tiền lương</p>
-                    </a>
-                </li>
-
-                <li class="nav-item menu-open">
+                <li class="nav-item">
                     <a href="{{ route('leave.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Thông kê ngày phép</p>
                     </a>
                 </li>
 
-                <li class="nav-item menu-open">
-                    <a href="{{ route('timesheets.monthSelection') }}" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>Bảng tính lương</p>
+                <li class="nav-item">
+                    <a href="{{ route('staffs.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-user-friends"></i>
+                        <p>Nhân sự <i class="fas fa-angle-left right"></i></p>
                     </a>
-                </li>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('organization.show') }}" class="nav-link">
+                                <i class="nav-icon fas fa-users-cog"></i>
+                                <p>Tổ chức nhân sự</p>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-trophy"></i>
+                                <p>Khen thưởng</p>
+                            </a>
+                        </li>
+                    </ul>
 
-                <li class="nav-item menu-open">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-trophy"></i>
-                        <p>Khen thưởng</p>
-                    </a>
-                </li>
-
-                <li class="nav-item menu-open">
-                    <a href="" class="nav-link">
-                        <i class="nav-icon fas fa-money-bill-alt"></i>
-                        <p>Tài chính</p>
-                    </a>
-                </li>
-                
-                <li class="nav-item menu-open">
-                    <a href="{{ route('organization.show') }}" class="nav-link">
-                        <i class="nav-icon fas fa-users-cog"></i>
-                        <p>Tổ chức nhân sự</p>
-                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-trophy"></i>
+                                <p>Tuyển dụng</p>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </nav>
