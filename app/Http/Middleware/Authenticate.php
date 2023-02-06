cc146bba162c760c001389d8b219b138f195a7f5
Hung NGUYEN <102758735+hungnguyen251@users.noreply.github.com>
2023-02-06 21:12:01 +0700

    Create authorize with authenticate middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,67 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\UnauthorizedException;
 
 class Authenticate extends Middleware
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * The authentication guard factory instance.
+     *
+     * @var \Illuminate\Contracts\Auth\Factory
+     */
+    protected $auth;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Factory  $auth
+     * @return void
+     */
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
      */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next, $role1 = null, $role2 = null, $role3 = null)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        $user = app('auth')->user();
+        
+        if ('active' != $user->status) {
+            app('auth')->logout();
+            throw new UnauthorizedException();
         }
+
+        if ('super_admin' != $user->decentralization && $role1 != null && $role1 != $user->decentralization 
+            && ($role2 == null || $role2 != $user->decentralization)
+            && ($role3 == null || $role3!= $user->decentralization)) {
+            throw new UnauthorizedException();
+        }
+
+        return $next($request);
     }
+
+    // /**
+    //  * Get the path the user should be redirected to when they are not authenticated.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @return string|null
+    //  */
+    // protected function redirectTo($request)
+    // {
+    //     if (! $request->expectsJson()) {
+    //         return route('login');
+    //     }
+    // }
 }
