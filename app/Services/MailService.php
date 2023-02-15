335c08783234c32b9e3d7e271bcd0addfc77e0d8
hungnguyen251 <102758735+hungnguyen251@users.noreply.github.com>
2023-02-16 00:53:41 +0700

    Update cc and bcc when send email

--- app/Services/MailService.php
+++ app/Services/MailService.php
@@ -12,25 +12,52 @@ class MailService
         $subject = $data['subject'];
         $to = $data['to'];
         $message = $data['message'];
-        
+        $ccEmails = !empty($data['cc']) ? $data['cc'] : '';
+        $bccEmails = !empty($data['bcc']) ? $data['bcc'] : '';
+
         if (true === Config::get('app.use_sendgrid')) {
-            return $this->sendgrid($to, $subject, $message);
+            return $this->sendgrid($to, $subject, $message, $ccEmails, $bccEmails);
         } else {
             return redirect()->back()->with('failed', 'Chức năng gửi email không khả dụng');
         }
     }
 
-    public function sendgrid($sendTo, $subject, $message) {
+    public function sendgrid($sendTo, $subject, $message, $ccEmails, $bccEmails) {
         $email = new Mail();
         $email->setFrom(Config::get('app.email_default'), Config::get('app.email_name_default'));
         $email->addTo(
             $sendTo,
-            'Hung Nguyen',
+            '',
             [
                 'subject' => $subject,
                 "message" => $message
             ],
         );
+
+        if ($ccEmails != '') {
+            $ccEmails = explode(',', $ccEmails);
+            if (is_array($ccEmails)) {
+                $cc = [];
+                foreach($ccEmails as $key => $value) {
+                    $cc[trim($value)] = '';
+                }
+                
+                $email->addCcs($cc);
+            }
+        }
+
+        if ($bccEmails != '') {
+            $bccEmails = explode(',', $bccEmails);
+            if (is_array($bccEmails)) {
+                $bcc = [];
+                foreach($bccEmails as $key => $value) {
+                    $bcc[trim($value)] = '';
+                }
+                
+                $email->addBccs($bcc);
+            }
+        }
+
         $email->setTemplateId(Config::get('app.send_grid_template_id'));
         $sendgrid = new \SendGrid(Config::get('app.send_grid_token'));
 
