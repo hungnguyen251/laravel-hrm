7078df8dabb48ff08cc1f5ea4d74304f30306dfc
hungnguyen251 <102758735+hungnguyen251@users.noreply.github.com>
2023-03-05 02:54:10 +0700

    Create documents table for storage file txt,xlsx..

---
+++ database/migrations/2023_03_04_225512_create_documents_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('file_name');
+            $table->enum('type', array('contract'))->default('contract');
+            $table->tinyText('note')->nullable();
+            $table->integer('author_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            //
+        });
+    }
+}
